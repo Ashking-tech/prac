879fc2959b21954a5885cd5a29685e9cd609a8b1
Ashking <Ashking21@proton.me>
2025-11-13 08:27:40 +0530

    vai

--- games/BFSDFS.cpp
+++ games/BFSDFS.cpp
@@ -1,85 +1,110 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Graph {
-    int v;
-    int adj[10][10];
+    int vertices;
+    int adj[10][10]; // adjacency matrix
     bool visited[10];
 
-public:
-    Graph(int vertices) {
-        v = vertices;
-        for (int i = 0; i < v; i++)
-            for (int j = 0; j < v; j++)
-                adj[i][j] = 0;
+    public:
+    Graph(int v){
+        vertices =v;
+        // initialize adjacency matrix
+        for (int i=0;i<v;i++)
+          for (int j=0;j<v;j++)
+          adj[i][j]=0;
+
     }
+     // Add an edge between two vertices
+    void addEdge(int u,int v){
+        adj[u][v] =1;
+        adj[v][u] =1; //unidirected Graph
 
-    void addEdge(int u, int w) {
-        adj[u][w] = adj[w][u] = 1;
     }
+    void BFS(int start){
+        bool visited[10]= {false};
+        int queue[10],front=0 ,rear=0;
 
-    void BFS(int start) {
-        bool vis[10] = {false};
-        int q[10], front = 0, rear = 0;
-        vis[start] = true;
-        q[rear++] = start;
+        visited[start]= true;
+        queue[rear++]= start;
 
-        cout << "BFS: ";
-        while (front < rear) {
-            int node = q[front++];
-            cout << char(node + 'A') << " ";
-            for (int i = 0; i < v; i++) {
-                if (adj[node][i] && !vis[i]) {
-                    vis[i] = true;
-                    q[rear++] = i;
+        cout << "BFS Traversal: ";
+        while (front<rear){
+            int node =queue[front++];
+            cout << char(node+ 'A') << " ";
+
+             for (int i = 0; i < vertices; i++){
+                if (adj[node][i]==1 && !visited[i]) {
+                    visited[i] = true;
+                    queue[rear++] =i;
+
                 }
             }
+
         }
         cout << endl;
-    }
 
-    void DFSrec(int node, bool vis[]) {
-        vis[node] = true;
-        cout << char(node + 'A') << " ";
-        for (int i = 0; i < v; i++)
-            if (adj[node][i] && !vis[i])
-                DFSrec(i, vis);
-    }
 
+    }
+    void DFS_Recursion(int node,bool visited[]) {
+        visited[node]= true;
+        cout << char(node+ 'A') << " ";
+
+        for (int i =0;i<vertices;i++) {
+            if (adj[node][i]==1 && !visited[i]){
+                DFS_Recursion(i,visited);
+            }
+        }
+
+    }
     void DFS(int start) {
-        bool vis[10] = {false};
-        cout << "DFS: ";
-        DFSrec(start, vis);
+        bool visited[10]={false};
+        cout << "DFS Traversal: " << endl;
+        DFS_Recursion(start,visited);
         cout << endl;
-    }
 
-    void display() {
-        cout << "Adjacency Matrix:\n";
-        for (int i = 0; i < v; i++) {
-            for (int j = 0; j < v; j++)
-                cout << adj[i][j] << " ";
-            cout << endl;
-        }
     }
+    void displayMatrix() {
+        cout << "Adejancy Matrix:" << endl;
+
+        for(int i=0; i<vertices;i++){
+            for (int j=0;j<vertices;j++)
+            cout << adj[i][j] << " ";
+            cout << endl;
+
+        }
+     }
 };
 
-int main() {
-    int v, e;
-    cout << "Enter number of vertices: ";
+int main () {
+    int v,e;
+    cout << "Enter the number of locations (vvertices): " << endl;
     cin >> v;
+
     Graph g(v);
-    cout << "Enter number of edges: ";
+
+    cout << "Enter number of Connections (edges): " <<endl;
     cin >> e;
-    cout << "Enter edges (u v):\n";
-    for (int i = 0; i < e; i++) {
-        int u, w;
-        cin >> u >> w;
-        g.addEdge(u, w);
-    }
-    g.display();
-    int start;
-    cout << "Enter start vertex (0-" << v - 1 << "): ";
-    cin >> start;
-    g.BFS(start);
-    g.DFS(start);
+
+    cout << "Enter the edges: " << endl;
+      for (int i=0; i< e;i++){
+        int u, vtx;
+        cin >> u>> vtx;
+        g.addEdge(u,vtx);
+
+      }
+      g.displayMatrix();
+
+      int start;
+      cout << "Enter Starting Node: " <<endl;
+      cin>> start;
+
+
+      cout << " " << endl;
+      g.BFS(start);
+      g.DFS(start);
+
+      return 0;
+
+
 }
